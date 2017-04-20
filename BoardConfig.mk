3ac528efc1765da494e1e57494b4ace67c9b21da
Jason Riordan <jriordan001@gmail.com>
2017-04-19 20:38:36 -0400

    build in lineage 14.1
    Change-Id: I208c7e09a51f5c80a850dc79d8748c2bc619992d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,7 @@ TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/leeco/msm8996
-TARGET_KERNEL_CONFIG := zl1_defconfig
+TARGET_KERNEL_CONFIG := lineage_zl1_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # /proc/partitions * 2 * BLOCK_SIZE (512) = size in bytes
