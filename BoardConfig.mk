97a03dbb028ad0bad374bd82ec11062c2b5efe3f
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-03-02 21:22:27 +0100

    zl1: re-enable ntfs support
    Change-Id: Id2efd3a1fddf9a2ce334823cb1a42c72946b1730

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,5 +65,5 @@ TW_INCLUDE_CRYPTO := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_EXCLUDE_SUPERSU := true
 TW_EXTRA_LANGUAGES := true
-#TW_INCLUDE_NTFS_3G := true
+TW_INCLUDE_NTFS_3G := true
 TW_THEME := portrait_hdpi
